3370e48242a5f86ac54e75ab2b2adee37db36c50
mateasdorinko <matt@maaldo.com>
2025-12-16 15:14:46 -0600

    added some endpoints;

---
+++ src/MaaldoCom.Services.Api/Endpoints/MediaAlbums/GetMediaAlbumByIdentifierEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using MaaldoCom.Services.Api.Contracts.MediaAlbums;
+
+namespace MaaldoCom.Services.Api.Endpoints.MediaAlbums;
+
+public class GetMediaAlbumByIdentifierEndpoint : Endpoint<string, GetMediaAlbumByIdentifierResponse>
+{
+    public override void Configure()
+    {
+        Get("/media-albums/{identifier}");
+        ResponseCache(60);
+        AllowAnonymous();
+        Description(b => b.Produces(StatusCodes.Status404NotFound));
+    }
+    
+    public override async Task HandleAsync(string req, CancellationToken ct)
+    {
+        if (string.IsNullOrEmpty(req))
+        {
+            await Send.NotFoundAsync(ct);
+        }
+        else
+        {
+            await Send.OkAsync(new GetMediaAlbumByIdentifierResponse(), ct);
+        } 
+    }
+}
---
+++ src/MaaldoCom.Services.Api/Endpoints/MediaAlbums/ListMediaAlbumsEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using MaaldoCom.Services.Api.Contracts.MediaAlbums;
+
+namespace MaaldoCom.Services.Api.Endpoints.MediaAlbums;
+
+public class ListMediaAlbumsEndpoint : EndpointWithoutRequest<ListMediaAlbumsResponse>
+{
+    public override void Configure()
+    {
+        Get("/media-albums");
+        ResponseCache(60);
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await Send.OkAsync(new ListMediaAlbumsResponse(), ct);
+    }
+}
