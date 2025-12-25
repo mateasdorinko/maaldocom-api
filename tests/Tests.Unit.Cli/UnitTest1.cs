9a55885f4a23f87374d19fc962d15f8dddc1484e
Mateas Dorinko <matt@maaldo.com>
2025-12-25 02:15:00 -0600

    fix build pipe;

---
+++ tests/Tests.Unit.Cli/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Tests.Unit.Cli;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/Tests.Unit.Domain/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Tests.Unit.Domain;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
