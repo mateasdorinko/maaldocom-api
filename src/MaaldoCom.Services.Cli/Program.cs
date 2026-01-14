4e3326767fd5c96ec772950638356ab572ddee52
mateasdorinko <matt@maaldo.com>
2026-01-13 22:33:24 -0600

    asdf

--- src/MaaldoCom.Services.Cli/Program.cs
+++ src/MaaldoCom.Services.Cli/Program.cs
@@ -1,12 +1,25 @@
-﻿using MaaldoCom.Services.Cli;
+/*
+using MaaldoCom.Services.Cli.Commands;
+using MaaldoCom.Services.Cli.Infrastructure;
+using MaaldoCom.Services.Cli.MaaldoApi;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+using Spectre.Console.Cli;
 
-AnsiConsole.MarkupLine("[red bold]Hello world![/]");
-AnsiConsole.MarkupLine("Hello world!");
-AnsiConsole.MarkupLine("[red on white]Hello world![/]");
+var services = new ServiceCollection();
 
-Styles.WriteError("This is an error message.");
-Styles.WriteWarning("This is a warning message.");
+services.AddRefitClient<IMaaldoApiClient>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://app-maaldocomapi-tst-cus.azurewebsites.net"));
 
+var registrar = new TypeRegistrar(services);
+var app = new CommandApp(registrar);
+
+app.Configure(config =>
+{
+    config.AddCommand<KnowledgeCommand>("knowledge")
+        .WithDescription("List knowledge items from the API");
+});
+
+return await app.RunAsync(args);
+*/
 Console.ReadLine();
-AnsiConsole.Clear();
-
