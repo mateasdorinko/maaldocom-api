d165ec940c4ba0eb9226bce9b1284673a68d57cf
mateasdorinko <matt@maaldo.com>
2026-01-14 12:32:15 -0600

    swapped out the Microsoft OpenApi component for the FastEndpoints Swagger doc generator;

--- src/MaaldoCom.Services.Api/Endpoints/MediaAlbums/Models/PostMediumRequest.cs
+++ src/MaaldoCom.Services.Api/Endpoints/MediaAlbums/Models/PostMediumRequest.cs
@@ -2,5 +2,5 @@ namespace MaaldoCom.Services.Api.Endpoints.MediaAlbums.Models;
 
 public class PostMediumRequest
 {
-    
+    public string TempPropertyDeleteMeLater { get; set; } = string.Empty;
 }
--- src/MaaldoCom.Services.Api/Endpoints/Tags/Models/PostTagResponse.cs
+++ src/MaaldoCom.Services.Api/Endpoints/Tags/Models/PostTagResponse.cs
@@ -2,5 +2,5 @@ namespace MaaldoCom.Services.Api.Endpoints.Tags.Models;
 
 public class PostTagResponse
 {
-    
+    public string TempPropertyDeleteMeLater { get; set; } = string.Empty;
 }
