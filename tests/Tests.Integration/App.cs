a36f711e114817f471808bc5531803bd3890ee2c
Mateas Dorinko <matt@maaldo.com>
2026-03-07 11:37:50 -0600

    start int test scaffolding;

--- tests/Tests.Integration/App.cs
+++ tests/Tests.Integration/App.cs
@@ -1,13 +1,29 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Testcontainers.Azurite;
+using Testcontainers.LowkeyVault;
+using Testcontainers.MsSql;
 
 namespace Tests.Integration;
 
 public class App : AppFixture<Program>
 {
-    protected override ValueTask SetupAsync()
+    private readonly LowkeyVaultContainer _vaultContainer
+        = new LowkeyVaultBuilder("ghcr.io/lowkeyvault/lowkeyvault:latest").Build();
+    private readonly MsSqlContainer _dbContainer
+        = new MsSqlBuilder("mcr.microsoft.com/mssql/server:2022-latest").Build();
+    private readonly AzuriteContainer _blobContainer
+        = new AzuriteBuilder("mcr.microsoft.com/azure-storage/azurite:latest").Build();
+
+    protected override async ValueTask PreSetupAsync()
     {
-        // place one-time setup for the fixture here
-        return ValueTask.CompletedTask;
+
+    }
+
+    protected override async ValueTask SetupAsync()
+    {
+        await _vaultContainer.StartAsync();
+        await _dbContainer.StartAsync();
+        await _blobContainer.StartAsync();
     }
 
     protected override void ConfigureApp(IWebHostBuilder a)
@@ -20,9 +36,10 @@ public class App : AppFixture<Program>
         // do test service registration here
     }
 
-    protected override ValueTask TearDownAsync()
+    protected override async ValueTask TearDownAsync()
     {
-        // do cleanups here
-        return ValueTask.CompletedTask;
+        await _vaultContainer.StopAsync();
+        await _dbContainer.StopAsync();
+        await _blobContainer.StopAsync();
     }
 }
